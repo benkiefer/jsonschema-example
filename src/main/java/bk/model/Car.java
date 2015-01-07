ea4547f704323b05d7a6b7a5e71a639e64f0217a
Ben Kiefer <kieferfam@msn.com>
2015-01-06 20:52:29 -0600

    example with date format

--- src/main/java/bk/model/Car.java
+++ src/main/java/bk/model/Car.java
@@ -1,5 +1,6 @@
 package bk.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
@@ -8,11 +9,13 @@ import java.util.List;
 public class Car {
     @JsonProperty(required = true)
     private String make;
-    @JsonProperty(value = "boo",required = true)
+    @JsonProperty(required = true)
     private String model;
     private TitleStatus titleStatus;
     @JsonPropertyDescription("people authorized to drive a car")
     private List<Driver> drivers;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm'Z'")
+    private String purchaseDate;
 
     public String getMake() {
         return make;
@@ -44,5 +47,13 @@ public class Car {
 
     public void setDrivers(List<Driver> drivers) {
         this.drivers = drivers;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
     }
 }
